52f87cc7d0eb91fa182523484f8d07b9e6bf4b92
Filpor <filip.porebinski@o2.pl>
2022-01-23 18:44:19 +0100

    add_contact()_and_property_dlugosc

--- wizytowki.py
+++ wizytowki.py
@@ -6,13 +6,20 @@ class person:
         self.company=company
         self.job=job
         self.email=email
+        #Variables
+        self._dlugosc=len(self.name)
+    def contact(self):
+        print("Kontaktuje się z", self.name , self.job , self.email )
+    @property
+    def dlugosc(self):
+        return len(self.name)
 person1=person(name=fake.name(),company=fake.company(),job=fake.job(),email=fake.email())
 person2=person(name=fake.name(),company=fake.company(),job=fake.job(),email=fake.email())
 person3=person(name=fake.name(),company=fake.company(),job=fake.job(),email=fake.email())
 person4=person(name=fake.name(),company=fake.company(),job=fake.job(),email=fake.email())
 person5=person(name=fake.name(),company=fake.company(),job=fake.job(),email=fake.email())
-new_list=[person1,person2,person3,person4,person5]
-by_name=sorted(new_list, key=lambda person: person.name)
-by_email=sorted(new_list, key=lambda person: person.email)
+
+print(person4.name)
+print(person4._dlugosc)
 
 
